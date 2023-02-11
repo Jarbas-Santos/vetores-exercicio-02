56a364d3d508a0dcb1db09d30b76248a11f808e4
Jarbas-Santos <sjarbas333@gmail.com>
2023-02-11 14:49:25 -0300

    Valores, soma e média dos elementos de um vetor.

---
+++ Main.java
@@ -0,0 +1,36 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Quantos numeros voce vai digitar? ");
+        int n = sc.nextInt();
+        double vect[] = new double[n];
+
+        for (int i = 0; i < vect.length; i++) {
+            System.out.print("Digite um numero: ");
+            vect[i] = sc.nextDouble();
+        }
+
+        System.out.print("\nVALORES = ");
+        for (int i = 0; i < vect.length; i++) {
+            System.out.printf(" %.1f", vect[i]);
+        }
+        
+        double sumVect = 0.0;
+        for (int i = 0; i < n; i++) {
+            sumVect += vect[i];
+        }
+        System.out.printf("\nSOMA = %.2f%n", sumVect);
+
+        double average = sumVect / vect.length;
+        System.out.printf("MEDIA = %.2f%n", average);
+
+        sc.close();
+    }
+}
